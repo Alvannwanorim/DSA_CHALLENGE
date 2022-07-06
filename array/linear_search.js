4ac3715ab167078bc59e7186cee253715baf46e7
alvancode <alvan.nwanorim@babbangona.com>
2022-07-06 23:10:53 +0100

    linear search

--- array/linear_search.js
+++ array/linear_search.js
@@ -1,1 +1,9 @@
-function linearSearch(arr, target) {}
+function linearSearch(arr, target) {
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i];
+		if (element === target) return true;
+	}
+	return false;
+}
+
+console.log(linearSearch([ 1, 2, 3, 4, 5, 6 ], 6));
