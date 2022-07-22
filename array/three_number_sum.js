d376ce9df2ea2bf98d41dcaec361adba0567dcad
alvancode <alvan.nwanorim@babbangona.com>
2022-07-22 23:16:40 +0100

    arrays

---
+++ array/three_number_sum.js
@@ -0,0 +1,13 @@
+function threeNumberSum(nums) {
+	let res = [];
+	let left = 0;
+	let right = nums.length - 1;
+	//Sor the array
+	nums.sort((a, b) => {
+		return a - b;
+	});
+
+	for (let i = 0; i < nums.length - 1; i++) {
+		const element = nums[i];
+	}
+}
